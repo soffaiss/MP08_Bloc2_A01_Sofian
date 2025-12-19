c094dccf82f9882e137d2013326e3378077c96d6
soffaiss <sufiaan.28@gmail.com>
2025-12-19 22:25:03 +0100

    a
    a

--- lib/views/notes/afegeix_view.dart
+++ lib/views/notes/afegeix_view.dart
@@ -1,85 +1,10 @@
-/*
-import 'package:flutter/material.dart';
-import 'package:sqlite_flutter_crud/JsonModels/note_model.dart';
-import 'package:sqlite_flutter_crud/persistencia/dbhelper.dart';
-import 'package:sqlite_flutter_crud/settings/constants_view.dart';
-import 'package:sqlite_flutter_crud/views/notes/common_controls_view.dart';
 
-//import '../../settings/constants_view.dart';
-
-
-
-class Afegeix extends StatefulWidget {
-  const Afegeix({super.key});
-
-  @override
-  State<Afegeix> createState() => _AfegeixState();
-}
-
-class _AfegeixState extends State<Afegeix> {
-  //final textEditingControllerTitol = TextEditingController();
-  //final textEditingControllerContingut = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-
-  final commonControlsView = CommonControlsView();
-  final databaseHelper = DatabaseHelper();
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(ConstantsView.LABEL_AFEGIR_NOTA),
-        actions: [
-          _getSaveIconButton(),
-        ],
-      ),
-      body: Form(
-        //I forgot to specify key
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: [
-                commonControlsView.getTextFormFieldTitol(
-                    ConstantsView.LABEL_NOTA_TITOL, commonControlsView.textEditingControllerTitol),
-                commonControlsView.getTextFormFieldContingut(
-                    (ConstantsView.LABEL_NOTA_CONTINGUT), commonControlsView.textEditingControllerContingut),
-              ],
-            ),
-          )),
-    );
-  }
-
-  IconButton _getSaveIconButton() {
-    return IconButton(
-        onPressed: () {
-          if (formKey.currentState!.validate()) {
-            databaseHelper
-                .createNote(NoteModel(
-                noteTitle: commonControlsView.textEditingControllerTitol.text,
-                noteContent: commonControlsView.textEditingControllerContingut.text,
-                createdAt: DateTime.now().toIso8601String()))
-                .whenComplete(() {
-              Navigator.of(context).pop(true);
-            });
-          }
-        },
-        icon: Icon(Icons.save));
-  }
-}
-*/
-
-// afegeix_view.dart
 import 'package:flutter/material.dart';
 import 'package:sqlite_flutter_crud/JsonModels/note_model.dart'; // Conté LlibreModel
 import 'package:sqlite_flutter_crud/persistencia/dbhelper.dart';
 import 'package:sqlite_flutter_crud/settings/constants_view.dart';
 import 'package:sqlite_flutter_crud/views/notes/common_controls_view.dart';
 
-//import '../../settings/constants_view.dart';
-
-
 
 class Afegeix extends StatefulWidget {
   const Afegeix({super.key});
@@ -89,8 +14,8 @@ class Afegeix extends StatefulWidget {
 }
 
 class _AfegeixState extends State<Afegeix> {
-  //final textEditingControllerTitol = TextEditingController();
-  //final textEditingControllerContingut = TextEditingController();
+  final textEditingControllerTitol = TextEditingController();
+  final textEditingControllerContingut = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
   final commonControlsView = CommonControlsView();
@@ -107,7 +32,6 @@ class _AfegeixState extends State<Afegeix> {
         ],
       ),
       body: Form(
-        //I forgot to specify key
           key: formKey,
           child: Padding(
             padding: const EdgeInsets.all(10.0),
@@ -128,10 +52,10 @@ class _AfegeixState extends State<Afegeix> {
         onPressed: () {
           if (formKey.currentState!.validate()) {
             databaseHelper
-                .createLlibre(LlibreModel( // CANVI: createNote a createLlibre, NoteModel a LlibreModel
-                llibreTitol: commonControlsView.textEditingControllerTitol.text, // CANVI: noteTitle a llibreTitol
-                llibreSinopsi: commonControlsView.textEditingControllerContingut.text, // CANVI: noteContent a llibreSinopsi
-                llibreCreatedAt: DateTime.now().toIso8601String())) // CANVI: createdAt a llibreCreatedAt
+                .createLlibre(LlibreModel( 
+                llibreTitol: commonControlsView.textEditingControllerTitol.text, 
+                llibreSinopsi: commonControlsView.textEditingControllerContingut.text, 
+                llibreCreatedAt: DateTime.now().toIso8601String())) 
                 .whenComplete(() {
               Navigator.of(context).pop(true);
             });
